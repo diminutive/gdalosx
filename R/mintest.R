73912ed48a7367e6731ac265190d2b53e758f5fe
Michael Sumner <mdsumner@gmail.com>
2018-02-12 19:18:07 +1100

    simple R example

--- R/mintest.R
+++ R/mintest.R
@@ -6,8 +6,18 @@
 #' @return text
 #' @export
 #'
-#' @examples
-#' #mintest()
-mintest <- function() {
+gdalinfotest <- function() {
   system("gdalinfo --version", intern = TRUE)
+}
+
+#' nothing at all
+#'
+#' returns a string
+#' @return character
+#' @export
+#'
+#' @examples
+#' mintest()
+mintest <- function() {
+  "nothing"
 }
